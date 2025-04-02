868b8ea193c76e46345aa3f7bb5ac2024d35e6cb
Jeremy Diamond <socjeremyd@gmail.com>
2025-04-02 00:19:22 -0600

    Lighning fix 2

--- grindset/next.config.ts
+++ grindset/next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // Optional, but it's a good practice to enable React strict mode
-  swcMinify: true, // Enable SWC minification for faster builds
-  webpack(config) {
-    // Custom webpack configuration if needed, such as for handling custom fonts or CSS
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
