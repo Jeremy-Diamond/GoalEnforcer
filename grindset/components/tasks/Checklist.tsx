3ce734c7b7cc6b6a94c8f5140d58eb7f65602a1b
Jeremy Karl Diamond <91515902+Jeremy-Diamond@users.noreply.github.com>
2025-04-16 19:02:50 -0600

    Update Checklist.tsx
    Reverting to Cody's code

--- grindset/components/tasks/Checklist.tsx
+++ grindset/components/tasks/Checklist.tsx
@@ -1,61 +1,64 @@
-"use client";
+'use client';
 
 import { updateDaily } from "@/app/lib/actions";
-import { useEffect, useState, useTransition } from "react";
+import { useState, useTransition } from "react";
+import { useEffect } from "react";
 
 type ChecklistProps = {
-  tTitle: string;
-  goalId: string;
-  taskId: string;
-  dcId: string;
-  checked: boolean;
-  selectedDate: Date;
-};
+    tTitle: string;
+    goalId: string;
+    taskId: string;
+    dcId: string;
+    checked: boolean;
+    selectedDate: Date;
+}
 
-export default function Checklist({
-  goalId,
-  taskId,
-  dcId,
-  checked,
-  tTitle,
-  //selectedDate
-}: ChecklistProps) {
-  // useEffect(() => {
-  //     // This effect runs whenever the selectedDate changes
-  //     console.log("Selected Date changed:", selectedDate);
-  //   }, [selectedDate]);
+export default function Checklist ({
+    goalId, 
+    taskId, 
+    dcId, 
+    checked, 
+    tTitle,
+    //selectedDate
+}:ChecklistProps) {
 
-  const [isPending, startTransition] = useTransition();
-  const [isChecked, setIsChecked] = useState(checked);
+    // useEffect(() => {
+    //     // This effect runs whenever the selectedDate changes
+    //     console.log("Selected Date changed:", selectedDate);
+    //   }, [selectedDate]);
 
-  useEffect(() => {
-    setIsChecked(checked);
-  }, [checked]);
+    const [isPending, startTransition] = useTransition();
+    const [isChecked, setIsChecked] = useState(checked);
 
-  const updateDatabase = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const isitChecked = e.target.checked;
-    //console.log("ischecked type:", typeof isChecked);
-    startTransition(() => {
-      updateDaily(goalId, taskId, dcId, isitChecked);
-      setIsChecked(!isChecked);
-    });
-  };
+      useEffect(() => {
+        setIsChecked(checked);
+      }, [checked]);
 
-  //console.log("checked" + checked);
-  //console.log("ischecked" + isChecked);
-  //console.log
 
-  return (
-    <div className="flex">
-      {isPending && <p>Updating...</p>}
-      <input
-        title="updateDatabase"
-        type="checkbox"
-        className="mr-4"
-        checked={isChecked}
-        onChange={updateDatabase}
-      />
-      <div>{tTitle}</div>
-    </div>
-  );
+    const updateDatabase = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const isitChecked = e.target.checked;
+        //console.log("ischecked type:", typeof isChecked);
+        startTransition(() => {
+            updateDaily(goalId, taskId, dcId, isitChecked);
+            setIsChecked(!isChecked);
+        });
+    };
+
+    //console.log("checked" + checked);
+    //console.log("ischecked" + isChecked);
+    //console.log
+
+    return (
+        <div className="flex">
+            {isPending && <p>Updating...</p>}
+            <input 
+                title="updateDatabase"
+                type="checkbox"  
+                className="mr-4"
+                checked={isChecked} 
+                onChange={updateDatabase}
+            />
+            <div>{tTitle}</div>
+        </div>
+    )
 }
