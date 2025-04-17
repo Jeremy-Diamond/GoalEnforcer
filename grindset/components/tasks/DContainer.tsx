becb06152e6e4787a41ac96baeb67d32fa2af4a7
Jeremy Karl Diamond <91515902+Jeremy-Diamond@users.noreply.github.com>
2025-04-16 19:07:44 -0600

    Update DContainer.tsx

--- grindset/components/tasks/DContainer.tsx
+++ grindset/components/tasks/DContainer.tsx
@@ -8,7 +8,69 @@ type DContainerProps = {
   gdate: Date; // The selected date for displaying tasks
 };
 
-// Define the structure of a daily completion record
+// Define the structure of a daily completion recordimport { getCurrentUserGoals } from "@/app/lib/actions";
+import DailyChecklist from "./DailyChecklist";
+
+type DContainerProps = {
+    gdate: Date;
+}
+
+interface DailyCompletion {
+    _id: string;
+    completed: boolean;
+    dueDate: Date;
+}
+
+interface Task {
+    _id: string;
+    taskTitle: string;
+    dailyCompletion: DailyCompletion[];
+}
+
+interface Goal {
+    _id: string;
+    title: string;
+    tasks: Task[];
+}
+
+export default async function DContainer ({gdate }: DContainerProps) {
+    const todayDate = new Date();
+    const viewthedate = gdate;
+    const goal = await getCurrentUserGoals();
+    let dateMessage = "";
+    //console.log(gdate.toString());
+    //console.log(viewthedate.toDateString());
+    //console.log(viewthedate.getDay());
+
+    if ( viewthedate.toDateString() === todayDate.toDateString()){
+        dateMessage = "Today's Daily Tasks";
+    } else {
+        dateMessage = ((viewthedate.getMonth() + 1) + "/" + viewthedate.getDate() + "/" + viewthedate.getFullYear() + " Daily Tasks");
+    }
+        return (
+            <div className="rounded-lg border border-[#2D3748] bg-[#1E2132] text-[#F8FAFC] shadow-md transition-colors p-6 max-w-7xl w-full relative">
+                {/* upper portion */}
+                <div className="mb-4 md:flex ">
+                    <div>
+                        <h2 className="text-2xl font-bold text-center">{dateMessage}</h2>
+                        {/* <p>Your scheduled goals for today</p>  */}
+                    </div>
+                </div>
+
+                {/* goal task lists */}
+                <div className="md:flex ">
+                    {goal.map((goal:Goal) => {
+                        return (
+                        <div key={goal._id.toString()} className="mb-4 md:mr-4"> 
+                            <DailyChecklist gid={goal._id.toString()} ddate={gdate.toString()} />
+                        </div>
+                        );
+                    })}
+                </div>
+
+            </div>
+        )
+}
 interface DailyCompletion {
   _id: string; // Unique identifier for the daily completion record
   completed: boolean; // Indicates if the task was completed on the given date
